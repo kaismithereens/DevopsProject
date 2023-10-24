bcd3551f3c4e56c4a1946f2845932db1008a211e
Kai <kai.nevermind@gmail.com>
2023-10-24 11:14:50 +0200

    Added Capybara Selenium tests to page_spec.rb

--- spec/unit/page_spec.rb
+++ spec/unit/page_spec.rb
@@ -1,7 +1,27 @@
 require 'capybara'
 require 'capybara/dsl'
+require 'selenium-webdriver'
 
-Describe "Example page render unit tests" do
-	It "Should show the Exlore California logo" do
-	end
+include Capybara::DSL
+Capybara.app_host = "http://website" # Using Selenium; connect over network
+Capybara.run_server = false # Disable Rack since we are using Selenium.
+Capybara.register_driver :remote_selenium do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--no-default-browser-check')
+  options.add_argument('--disable-dev-shm')
+  Capybara::Selenium::Driver.new(
+    app,
+    :browser => :remote,
+    :url => "http://#{ENV['SELENIUM_HOST']}:#{ENV['SELENIUM_PORT']}/wd/hub",
+    :options => options
+  )
+end
+Capybara.default_driver = :remote_selenium
+Capybara.javascript_driver = :remote_selenium
+
+describe "Example page render unit tests" do
+  it "Should show the Exlore California logo" do
+   visit('/')
+   expect(page.has_selector? 'a.logo').to be_truthy
+  end
 end
